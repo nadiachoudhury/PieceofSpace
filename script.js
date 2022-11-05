44b6a5c7733febd15c2df8343b3128c88ebaf531
Afsana <42010124+afsanab@users.noreply.github.com>
2022-11-05 18:31:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const element = document.getElementById("sun1");
 element.addEventListener("click", function(){
-  document.getElementById("piece1").innerHTML = "Sun";
+  <!--document.getElementById("piece1").innerHTML = "Sun";-->
+  console.log("hello");
 });
